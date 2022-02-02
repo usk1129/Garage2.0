ac31d889803a1189a4deb414a9bc7102ad149d33
usk1129 <k_usko@yahoo.com>
2022-02-02 16:24:10 +0100

    more tests

--- Garage2.0Tests1/Controllers/ParkVehiclesControllerTests.cs
+++ Garage2.0Tests1/Controllers/ParkVehiclesControllerTests.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Garage2._0.Data;
 using Moq;
 using Xunit;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Garage2._0.Controllers.Tests
 {
@@ -21,7 +22,7 @@ namespace Garage2._0.Controllers.Tests
             var mockRepo = new Mock<Garage2_0Context>();
 
             var controller = new ParkVehiclesController(mockRepo.Object);
-            controller.ModelState.AddModelError("Error", "Some Error");
+            controller.ModelState.AddModelError("Error", "invalid model");
 
             //Act
 
@@ -30,5 +31,33 @@ namespace Garage2._0.Controllers.Tests
             //Assert
             Assert.IsNotNull(result);
         }
+
+        [Fact]
+        public async Task Edit(int? id)
+        {
+            var mockRepo = new Mock<Garage2_0Context>();
+            // Arrange
+            var controller = new ParkVehiclesController(mockRepo.Object);
+
+            // Act
+            ViewResult result = await controller.Edit(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [Fact]
+        public async Task Delete(int? id)
+        {
+            var mockRepo = new Mock<Garage2_0Context>();
+            // Arrange
+            var controller = new ParkVehiclesController(mockRepo.Object);
+
+            // Act
+            ViewResult result = await controller.Delete(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
     }
 }
