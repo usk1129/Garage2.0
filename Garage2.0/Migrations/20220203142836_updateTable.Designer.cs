63c6998760b3915421131703f37ed3be9d5570a9
Ashutosh Ranjan <ashutosh.svea@gmail.com>
2022-02-03 15:52:24 +0100

    Added slot capacity check for bus and truck. added submit button disable for full capacity garage. add new seed data. makde vehicle type and garage slot uneditable.

--- Garage2.0/Migrations/20220203142836_updateTable.Designer.cs
+++ Garage2.0/Migrations/20220203142836_updateTable.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Garage2._0.Migrations
 {
     [DbContext(typeof(Garage2_0Context))]
-    [Migration("20220202190508_SeedData")]
-    partial class SeedData
+    [Migration("20220203142836_updateTable")]
+    partial class updateTable
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -50,8 +50,9 @@ namespace Garage2._0.Migrations
                         .HasMaxLength(30)
                         .HasColumnType("nvarchar(30)");
 
-                    b.Property<int>("ParkingSlot")
-                        .HasColumnType("int");
+                    b.Property<string>("ParkingSlot")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("RegNumber")
                         .IsRequired()
