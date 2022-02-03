3c7cf1bac98c3407f2bb1694e980d27927e9d8ec
mhk <mackanhk@hotmail.com>
2022-02-03 12:15:22 +0100

    statistics page and logic

---
+++ Garage2.0/Models/StatisticsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Garage2._0.Helpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Garage2._0.Models
+{
+    public class StatisticsViewModel
+    {
+        public int AmountOfVehicles { get; set; }
+        public int AmountOfWheels { get; set; }
+        public int CurrentFees { get; set; }
+        public List<VehicleTypeHelper> VehicleTypeAmount { get; set; }
+    }
+}
