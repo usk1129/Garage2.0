6d1dee8d4fbf86a35c6fd34a5a49311f5254cc46
Ashutosh Ranjan <ashutosh.svea@gmail.com>
2022-02-16 20:36:27 +0100

    ParkingSpot Class Entity,Table and migration added.

--- Garage2.0/Data/Garage2_0Context.cs
+++ Garage2.0/Data/Garage2_0Context.cs
@@ -18,5 +18,7 @@ namespace Garage2._0.Data
         public DbSet<Garage2._0.Models.ParkVehicle> ParkVehicle { get; set; }
 
         public DbSet<Garage2._0.Models.Member> Member { get; set; }
+
+        public DbSet<Garage2._0.Models.ParkingSpot> ParkingSpot { get; set; }
     }
 }
