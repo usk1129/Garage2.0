3cc2c29b02bbb3d783a4800bf65d2b98713ed9cb
Jesse Varis <jessevaris@hotmail.com>
2022-02-17 11:42:08 +0100

    Start on check personnr

---
+++ Garage2.0/Models/CheckPersonNr.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.OData.Edm;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace Garage2._0.Models
+{
+    public class CheckPersonNr : ValidationAttribute
+    {
+        // public readonly DateTime PersonNr;
+        //private DateTime dt;
+
+        //public CheckPersonNr()
+        //{
+        //}
+
+        public override bool IsValid(object? value)
+        {
+            //19500101-1111
+            //195001011111
+            if (value is string personNr)
+            {
+                var parts = personNr.Trim().Split('-');
+
+                if (DateTime.TryParseExact(parts[0], new[] { "yyMMdd", "yyyyMMdd" }, new CultureInfo("sv-SE"), DateTimeStyles.None, out dt))
+                {
+
+                }
+
+            }
+
+            return false;
+
+        }
+    }
+}
