35fedc32a6f7740f908d94359f39ade9520b9254
mhk <mackanhk@hotmail.com>
2022-02-19 14:33:51 +0100

    get empty spots selectlist

--- Garage2.0/Services/ParkingSpotSelectListService.cs
+++ Garage2.0/Services/ParkingSpotSelectListService.cs
@@ -13,6 +13,17 @@ namespace Garage2._0.Services
             this.db = db;
         }
 
+        public async Task<IEnumerable<SelectListItem>> GetEmptyParkingSpotsAsync()
+        {
+
+            return await db.ParkingSpot.Where(v => v.ParkVehicleID == null)
+           .Select(v => v)
+           .Select(t => new SelectListItem
+           {
+               Text = t.ParkingSpotNr.ToString(),
+               Value = t.ParkingSpotNr.ToString()
+           }).ToListAsync();
+        }
 
         public async Task<IEnumerable<SelectListItem>> GetParkingSpotsAsync()
         {
