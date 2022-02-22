e74822d98e9f52a8a924353db0438b49f2d80de8
Jesse Varis <jessevaris@hotmail.com>
2022-02-22 10:12:24 +0100

    Changed text

--- Garage2.0/Models/Member.cs
+++ Garage2.0/Models/Member.cs
@@ -1,17 +1,23 @@
-﻿namespace Garage2._0.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage2._0.Models
 {
     public class Member
     {
         public int Id { get; set; }
 
         public string? Avatar { get; set; }
+        [Display(Name = "First Name")]
 
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
 
         public string LastName { get; set; }
 
+        [Display(Name = "Person Number")]
         [CheckPersonNr]
         public string PersonNumber { get; set; }
+        [Display(Name = "Age")]
         public int Age { get; set; }
 
         // Nav Prop
--- Garage2.0/Models/MemberViewModel.cs
+++ Garage2.0/Models/MemberViewModel.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string? Avatar { get; set; }
-
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
